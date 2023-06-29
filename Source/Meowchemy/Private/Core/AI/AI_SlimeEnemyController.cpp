3680957849920751899d1bdd6e71f233605814a8
Victor <gd05victor@thecea.ca>
2023-06-28 18:02:50 -0700

    Wip progress

--- Source/Meowchemy/Private/Core/AI/AI_SlimeEnemyController.cpp
+++ Source/Meowchemy/Private/Core/AI/AI_SlimeEnemyController.cpp
@@ -29,33 +29,41 @@ void AAI_SlimeEnemyController::Tick(float DeltaSeconds)
 
 	const FVector StartVector = GetPawn()->GetActorLocation();
 	const FVector EndVector = UGameplayStatics::GetPlayerCharacter(GetWorld(), 0)->GetActorLocation();
-
 	// EndVector.Normalize();
-	if (EndVector.Length() <= TraceDistance)
+
+	TArray<FHitResult> HitResults;
+	FCollisionShape ColShape = FCollisionShape::MakeSphere(TraceRadius);
+	FCollisionQueryParams QueryParams;
+	const AActor* SelfActor = GetPawn();
+	QueryParams.AddIgnoredActor(SelfActor);
+
+	bool Hit = GetWorld()->SweepMultiByChannel(HitResults, StartVector, EndVector, FQuat::Identity,
+	                                           ECC_Visibility, ColShape, QueryParams);
+
+	DrawDebugSphere(GetWorld(), StartVector, TraceRadius, 12, FColor::Orange, false);
+	DrawDebugLine(GetWorld(), StartVector, EndVector, FColor::Orange, false);
+
+	const FVector Direction = EndVector - StartVector;
+	if (Direction.Length() <= TraceDistance)
 	{
-		TArray<FHitResult> HitResults;
-
-		FCollisionShape ColShape = FCollisionShape::MakeSphere(TraceRadius);
-
-		bool Hit = GetWorld()->SweepMultiByChannel(HitResults, StartVector, EndVector, FQuat::Identity,
-		                                           ECC_Visibility, ColShape);
-
-		DrawDebugSphere(GetWorld(), StartVector, TraceRadius, 12, FColor::Orange, false);
-		DrawDebugLine(GetWorld(), StartVector, EndVector, FColor::Orange, false);
-
+			DrawDebugLine(GetWorld(), StartVector, EndVector, FColor::Red, false);
+			PlayerLocated = true;
+		
 		if (Hit)
 		{
+
 			for (FHitResult const HitResult : HitResults)
 			{
-				if (const AActor* HitActor = HitResult.GetActor(); HitActor->ActorHasTag("Player"))
+				DrawDebugSphere(GetWorld(), HitResult.Location, TraceRadius / 2, 12, FColor::Red, false);
+
+				const AActor* HitActor = HitResult.GetActor();
+				if (HitActor->ActorHasTag("Player"))
 				{
-					GEngine->AddOnScreenDebugMessage(-1, 0.1f, FColor::Orange,
-					                                 FString::Printf(
+					GEngine->AddOnScreenDebugMessage(-1, 0.1f, FColor::Orange, FString::Printf(
 						                                 TEXT("Hit: %s"), *HitResult.GetActor()->GetName()));
-					PlayerLocated = true;
-					break;
+					// break;
 				}
-				PlayerLocated = false;
+				// PlayerLocated = false;
 			}
 		}
 	}
